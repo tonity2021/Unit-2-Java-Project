44ae8104517598d3c904736b6245d799cc363f33
Toni-Ty <Antonique2479@yahoo.com>
2022-01-31 01:26:26 -0600

    built new project

---
+++ labs/Unit-2-Java-Project/src/main/java/com/example/unit2javaproject/Unit2JavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.unit2javaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2JavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Unit2JavaProjectApplication.class, args);
+    }
+
+}
---
+++ labs/Unit-2-Java-Project/src/test/java/com/example/unit2javaproject/Unit2JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit2javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit2JavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
