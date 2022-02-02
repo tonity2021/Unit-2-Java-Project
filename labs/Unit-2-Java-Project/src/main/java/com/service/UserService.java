2ea3c59cf4120f3934b893925e88febe068c933d
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 18:29:51 -0600

    Added methods to UserService class for creating and finding a user

--- labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
+++ labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
@@ -1,6 +1,8 @@
 package com.service;
 
 
+import com.exceptions.InformationExistException;
+import com.model.User;
 import com.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,4 +20,19 @@ public class UserService {
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    public User createUser(User userObject) {
+        System.out.println("service calling createUser ==>");
+        if (!userRepository.existsByEmailAddress(userObject.getEmail())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("user with email address " + userObject.getEmail() +
+                    " already exists");
+        }
+    }
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmailAddress(email);
+    }
 }
+
