470be4cb96274c1694a406ab8857e263f40000a8
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 19:57:55 -0600

    Add annotations to UserController and import classes

--- labs/Unit-2-Java-Project/src/main/java/com/controller/UserController.java
+++ labs/Unit-2-Java-Project/src/main/java/com/controller/UserController.java
@@ -1,5 +1,26 @@
 package com.controller;
 
-    public class UserController {
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/auth/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
