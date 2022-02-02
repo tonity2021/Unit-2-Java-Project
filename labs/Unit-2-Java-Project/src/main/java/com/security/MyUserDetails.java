5318ad06d72d59dc85270893d6c0dfb5666e0476
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 18:46:46 -0600

    Implemented MyUserDetails class with getters.

--- labs/Unit-2-Java-Project/src/main/java/com/security/MyUserDetails.java
+++ labs/Unit-2-Java-Project/src/main/java/com/security/MyUserDetails.java
@@ -14,12 +14,51 @@ public class MyUserDetails implements UserDetails {
     private String password;
     private String emailAddress;
 
+    public MyUserDetails
 
-
-    public MyUserDetails(User user) {
+            (User user) {
         this.user = user;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return new HashSet<GrantedAuthority>();
     }
+
+    @Override
+    public String getPassword(){
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername(){
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
 
 
 
