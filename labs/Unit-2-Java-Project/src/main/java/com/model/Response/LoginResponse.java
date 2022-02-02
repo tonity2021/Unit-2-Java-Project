3a20c60eaf967ad2cf62aa595ea760c626d1aa63
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 20:38:18 -0600

    Add JWT getter and setter in LoginResponse class

--- labs/Unit-2-Java-Project/src/main/java/com/model/Response/LoginResponse.java
+++ labs/Unit-2-Java-Project/src/main/java/com/model/Response/LoginResponse.java
@@ -3,5 +3,17 @@ package com.model.Response;
 
 public class LoginResponse {
 
+    private String JWT;
 
+    public String getJWT() {
+        return JWT;
+    }
+
+    public LoginResponse(String JWT) {
+        this.JWT = JWT;
+    }
+
+    public void setJWT(String JWT) {
+        this.JWT = JWT;
+    }
 }
