2dc620d13f9f11adee862b0372b5db6f3e3c6828
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 19:54:49 -0600

    Add a request package in model and log-in request class

---
+++ labs/Unit-2-Java-Project/src/main/java/com/model/Request/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.model.Request;
+
+
+public class LoginRequest {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
