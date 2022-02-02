975360d734efb1b51aac922719e3b9cff4433ad2
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 18:40:53 -0600

    Implemented MyUserDetailsService class and imported classes.

--- labs/Unit-2-Java-Project/src/main/java/com/security/MyUserDetailsService.java
+++ labs/Unit-2-Java-Project/src/main/java/com/security/MyUserDetailsService.java
@@ -1,5 +1,27 @@
 package com.security;
 
-public class MyUserDetailsService {
+import com.model.User;
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
 
 }
