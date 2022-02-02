0793f1eb5854f7dbd61f947989c087cc318158fc
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 18:25:50 -0600

    Added annotations and imported classes into UserService class

--- labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
+++ labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
@@ -1,6 +1,21 @@
 package com.service;
 
+
+import com.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 
+    private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
