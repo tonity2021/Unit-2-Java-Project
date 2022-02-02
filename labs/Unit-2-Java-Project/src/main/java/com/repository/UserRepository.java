fc8fe80e1cf61e1981a27e440e89c463ca745912
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 18:10:34 -0600

    Created UserRepository and added login and register methods

--- labs/Unit-2-Java-Project/src/main/java/com/repository/UserRepository.java
+++ labs/Unit-2-Java-Project/src/main/java/com/repository/UserRepository.java
@@ -1,2 +1,15 @@
 package com.repository;
 
+
+import com.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //to register
+    boolean existsByEmailAddress(String userEmailAddress);
+
+    //to login
+    User findUserByEmailAddress(String userEmailAddress);
+
+}
--- labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
+++ labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
@@ -2,4 +2,5 @@ package com.service;
 
 public class UserService {
 
+
 }
