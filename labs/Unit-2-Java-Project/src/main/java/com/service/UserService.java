9c865f1c34bb98a2cd23cb2af95a397cd61cd17c
Toni-Ty <Antonique2479@yahoo.com>
2022-02-01 20:43:33 -0600

    update UserService class annotations and import classes get and set UserRepository

--- labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
+++ labs/Unit-2-Java-Project/src/main/java/com/service/UserService.java
@@ -1,12 +1,17 @@
 package com.service;
 
 
-import com.exceptions.InformationExistException;
-import com.model.User;
 import com.repository.UserRepository;
+import com.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
@@ -14,25 +19,20 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User createUser(User userObject) {
-        System.out.println("service calling createUser ==>");
-        if (!userRepository.existsByEmailAddress(userObject.getEmail())) {
-            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
-            return userRepository.save(userObject);
-        } else {
-            throw new InformationExistException("user with email address " + userObject.getEmail() +
-                    " already exists");
-        }
-    }
-    public User findUserByEmailAddress(String email) {
-        return userRepository.findUserByEmailAddress(email);
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JWTUtils jwtUtils;
 }
 
