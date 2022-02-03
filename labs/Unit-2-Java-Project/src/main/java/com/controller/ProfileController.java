a0fc9170e738ec96c3f086035de809e9bc4fddc0
Toni-Ty <Antonique2479@yahoo.com>
2022-02-03 16:34:31 -0600

    remove unnecessary imported classes

--- labs/Unit-2-Java-Project/src/main/java/com/controller/ProfileController.java
+++ labs/Unit-2-Java-Project/src/main/java/com/controller/ProfileController.java
@@ -1,20 +1,11 @@
 package com.controller;
 
-import com.exceptions.InformationExistException;
-import com.exceptions.InformationNotFoundException;
-import com.model.Reservation;
 import com.model.UserProfile;
-import com.repository.ReservationRepository;
-import com.security.MyUserDetails;
 import com.service.ProfileService;
 import com.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/auth")
