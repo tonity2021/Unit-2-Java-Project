13947defa759d197fbd92b41e487f228b75e0260
Toni-Ty <Antonique2479@yahoo.com>
2022-02-03 16:29:48 -0600

    create UserProfileRepository and add method

---
+++ labs/Unit-2-Java-Project/src/main/java/com/repository/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package com.repository;
+
+import com.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    UserProfile findByUserId(Long UserId);
+
+}
